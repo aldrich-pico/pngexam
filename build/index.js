134c320c0d6d3857610515a3e8487239589e7ea5
aldrich-pico <62074615+aldrich-pico@users.noreply.github.com>
2021-02-21 18:13:29 +0800

    Initial setup of scene and inputs

--- build/index.js
+++ build/index.js
@@ -1,32 +1,102 @@
 "use strict";
+const CAMERA_FOV = 75;
+const CAMERA_ASPECT_RATIO = window.innerWidth / window.innerHeight;
+const CAMERA_NEAR_PLANE = 0.1;
+const CAMERA_FAR_PLANE = 1000;
 let camera;
 let scene;
 let renderer;
-let geometry;
-let material;
-let mesh;
-const camera_fov = 75;
-const camera_aspect_ratio = window.innerWidth / window.innerHeight;
-const camera_near_plane = 0.1;
-const camera_far_plane = 1000;
-init();
+let geometry1;
+let geometry2;
+let material1;
+let material2;
+let mesh1;
+let mesh2;
+start();
+function start() {
+    init();
+    setupScene();
+    setupActors();
+    setupRenderer();
+    setupEvents();
+}
 function init() {
-    camera = new THREE.PerspectiveCamera(camera_fov, camera_aspect_ratio, camera_near_plane, camera_far_plane);
-    camera.position.z = 5;
-    scene = new THREE.Scene();
-    geometry = new THREE.BoxGeometry(2, 2, 2);
-    material = new THREE.MeshNormalMaterial();
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    mesh.position.x = 1;
-    mesh.position.z = -2;
     renderer = new THREE.WebGLRenderer();
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(CAMERA_FOV, CAMERA_ASPECT_RATIO, CAMERA_NEAR_PLANE, CAMERA_FAR_PLANE);
+    geometry1 = new THREE.BoxGeometry(1, 1, 1);
+    geometry2 = new THREE.DodecahedronBufferGeometry(0.5);
+    material1 = new THREE.MeshNormalMaterial();
+    material2 = new THREE.MeshNormalMaterial();
+    mesh1 = new THREE.Mesh(geometry1, material1);
+    mesh2 = new THREE.Mesh(geometry2, material2);
+}
+function setupScene() {
+    camera.position.z = 5;
+}
+function setupActors() {
+    scene.add(mesh1);
+    scene.add(mesh2);
+    mesh1.position.x = -1;
+    mesh2.position.x = 1;
+}
+function setupRenderer() {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setAnimationLoop(animation);
+    renderer.setAnimationLoop(update);
     document.body.appendChild(renderer.domElement);
 }
-function animation(time) {
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
+function setupEvents() {
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('contextmenu', onContextMenu, false);
+}
+function update(time) {
+    mesh1.rotation.x = time / 2000;
+    mesh1.rotation.y = time / 1000;
+    mesh2.rotation.x = time / 2000;
+    mesh2.rotation.y = time / 1000;
     renderer.render(scene, camera);
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function onDocumentMouseDown(event) {
+    event.preventDefault();
+    switch (event.which) {
+        case 1:
+            console.log("Event 1");
+            break;
+        case 2:
+            console.log("Event 2");
+            break;
+        case 3:
+            console.log("Event 3");
+            break;
+    }
+}
+function onDocumentMouseUp(event) {
+    event.preventDefault();
+    switch (event.which) {
+        case 1:
+            console.log("Event Up 1");
+            break;
+        case 2:
+            console.log("Event Up 2");
+            break;
+        case 3:
+            console.log("Event Up 3");
+            break;
+    }
+}
+function onDocumentMouseMove(event) {
+    event.preventDefault();
+    console.log("Mouse X: " + event.x);
+    console.log("Mouse Y: " + event.y);
+}
+function onContextMenu(event) {
+    event.preventDefault();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,129 @@
+const CAMERA_FOV: number = 75;
+const CAMERA_ASPECT_RATIO: number = window.innerWidth / window.innerHeight;
+const CAMERA_NEAR_PLANE: number = 0.1;
+const CAMERA_FAR_PLANE: number = 1000;
+
 let camera: THREE.PerspectiveCamera;
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
-let geometry: THREE.BoxGeometry;
-let material: THREE.MeshNormalMaterial;
-let mesh: THREE.Mesh;
 
-const camera_fov: number = 75;
-const camera_aspect_ratio: number = window.innerWidth / window.innerHeight;
-const camera_near_plane: number = 0.1;
-const camera_far_plane: number = 1000;
+let geometry1: THREE.BoxGeometry;
+let geometry2: THREE.DodecahedronBufferGeometry;
 
-init();
+let material1: THREE.MeshNormalMaterial;
+let material2: THREE.MeshNormalMaterial;
+
+let mesh1: THREE.Mesh;
+let mesh2: THREE.Mesh;
+
+start();
+
+function start() {
+	init();
+	setupScene();
+	setupActors();
+	setupRenderer();
+	setupEvents();
+}
 
 function init() {
-
-	camera = new THREE.PerspectiveCamera(camera_fov, camera_aspect_ratio, camera_near_plane, camera_far_plane);
-	camera.position.z = 5;
+	renderer = new THREE.WebGLRenderer();
 
 	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(CAMERA_FOV, CAMERA_ASPECT_RATIO, CAMERA_NEAR_PLANE, CAMERA_FAR_PLANE);
 
-	geometry = new THREE.BoxGeometry(2, 2, 2);
-	material = new THREE.MeshNormalMaterial();
+	geometry1 = new THREE.BoxGeometry(1, 1, 1);
+	geometry2 = new THREE.DodecahedronBufferGeometry(0.5);
 
-	mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
-	mesh.position.x = 1;
-	mesh.position.z = -2;
+	material1 = new THREE.MeshNormalMaterial();
+	material2 = new THREE.MeshNormalMaterial();
 
-	renderer = new THREE.WebGLRenderer();
+	mesh1 = new THREE.Mesh(geometry1, material1);
+	mesh2 = new THREE.Mesh(geometry2, material2);
+}
+
+function setupScene() {
+	camera.position.z = 5;
+}
+
+function setupActors() {
+	scene.add(mesh1);
+	scene.add(mesh2);
+
+	mesh1.position.x = -1;
+	mesh2.position.x = 1;
+}
+
+function setupRenderer() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setAnimationLoop(animation);
+	renderer.setAnimationLoop(update);
 	document.body.appendChild(renderer.domElement);
 }
 
-function animation(time: number) {
+function setupEvents() {
+	window.addEventListener('resize', onWindowResize, false);
+	document.addEventListener('mousedown', onDocumentMouseDown, false);
+	document.addEventListener('mouseup', onDocumentMouseUp, false);
+	document.addEventListener('mousemove', onDocumentMouseMove, false);
+	document.addEventListener('contextmenu', onContextMenu, false);
+}
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+function update(time: number) {
+	mesh1.rotation.x = time / 2000;
+	mesh1.rotation.y = time / 1000;
+
+	mesh2.rotation.x = time / 2000;
+	mesh2.rotation.y = time / 1000;
 
 	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onDocumentMouseDown(event: MouseEvent) {
+	event.preventDefault();
+
+	switch (event.which) {
+		case 1:
+			console.log("Event 1");
+			break;
+		case 2:
+			console.log("Event 2");
+			break;
+		case 3:
+			console.log("Event 3");
+			break;
+	}
+}
+
+function onDocumentMouseUp(event: MouseEvent) {
+	event.preventDefault();
+
+	switch (event.which) {
+		case 1: 
+			console.log("Event Up 1");
+			break;
+		case 2:
+			console.log("Event Up 2");
+			break;
+		case 3:
+			console.log("Event Up 3");
+			break;
+	}
+}
+
+function onDocumentMouseMove(event: MouseEvent) {
+	event.preventDefault();
+
+	console.log("Mouse X: " + event.x);
+	console.log("Mouse Y: " + event.y);
+}
+
+function onContextMenu(event: Event) {
+	event.preventDefault();
 }
